abb80051861ddc38ef614b1fc9b5f4aadf47730c
@timlanz25 <timlanz.toolkt@gmail.com>
2025-01-30 17:19:16 +0800

    Update __manifest__.py, occ_bir_2307.prpt, and occ_payroll_report.prpt

--- occ_detailed_payroll_reports/__manifest__.py
+++ occ_detailed_payroll_reports/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','occ_configurations','occ_custom_payroll', 'pentaho_reports_odoo'],
+    'depends': ['base','occ_configurations','occ_custom_payroll', 'pentaho_reports_odoo', 'hr'],
 
     # always loaded
     'data': [
